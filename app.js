f35baff2b9542f5ba90bef34f5016c7117468019
gauravdattjoshi <letslearncbse0@gmail.com>
2021-06-11 20:33:00 +0530

    Made Changes

--- app.js
+++ app.js
@@ -8,29 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 
 //MailChimp Details
 mailChimp.setConfig({
-    apiKey: process.env.MAILCHIMP_API_KEY,
+    apiKey: process.env.MAILCHIMP_API_KEY + "dmdm",
     server: process.env.SERVER_PREFIX,
 
 });
-console.log(process.env.MAILCHIMP_API_KEY,process.env.SERVER_PREFIX,process.env.LIST_ID);
-
-async function run(email_address, FNAME, LNAME) {
-    console.log(email_address, LNAME);
-    try {
-        const response = await mailChimp.lists.addListMember(process.env.LIST_ID, {
-            email_address,
-            status: 'subscribed',
-            merger_fields: {
-                FNAME:FNAME, LNAME:LNAME
-            }
-        })
-        return response;
-    } catch (error) {
-        console.log(error);
-    }
-
-}
-
 
 
 //homepage
@@ -39,21 +20,37 @@ app.get('', (req, res) => {
 });
 
 
-
 //post request
+app.post('/submit', async (req, res) => {
 
-
-app.post('/submit', async(req, res) => {
     const body = req.body;
-    console.log(req.body.email_address, body.email_address);
-    const result = await run(
-        body.email_address,
-        body.first_name, body.last_name
-    );
-    res.send(`<h1>Successfully added contact as an audience member. The contact's id is ${result.id}</h1>`)
+    try {
+
+        await mailChimp.lists.addListMember(process.env.LIST_ID, {
+            email_address: body.email_address,
+            status: 'subscribed',
+            merger_fields: {
+                FNAME: body.first_name, LNAME: body.last_name
+            }
+        })
+
+
+
+        if (res.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html')
+        } else {
+            res.sendFile(__dirname + '/failure.html')
+        }
+
+    } catch (error) {
+        res.sendFile(__dirname + '/failure.html');
+    }
 
 })
 
+app.post('/failure', (req, res) => {
+    res.redirect('/');
+})
 
 
 //start server
